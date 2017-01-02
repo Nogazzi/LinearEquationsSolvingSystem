acaed92747ead05a665ddc6deeb39478c48a57e2
Nogazzi <p.noganski@gmail.com>
2017-01-02 16:08:07 +0100

    First base commit

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Nogaz on 02.01.2017.
+ */
+public class MainFrame extends JFrame {
+
+    public MainFrame(){
+
+    }
+}
---
+++ src/gui/ResultsDataPanel.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Nogaz on 02.01.2017.
+ */
+public class ResultsDataPanel extends JPanel {
+    public ResultsDataPanel(){
+
+    }
+}
